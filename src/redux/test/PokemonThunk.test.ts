cded3f329754940bebb0abb33f13f278d347a570
Gaterise <anexaki@gmail.com>
2024-01-05 17:51:26 +0900

    thunkのテストを追加

---
+++ src/redux/test/PokemonThunk.test.ts
@@ -0,0 +1,39 @@
+import {vi} from 'vitest'
+import {getAndSetPokemonThunk} from "../PokemonThunk.ts";
+import axios from "axios";
+
+vi.mock('axios')
+
+describe('PokemonThunk Tests', () => {
+    test('getAndSetPokemonThunkを呼んだ時、axiosを正しい引数で呼んでいること', async () => {
+        vi.spyOn(Math, 'floor').mockReturnValue(0);
+
+        // コンポーネント外でdispatch無しでthunk関数を呼ぶにはこのようにする（特許出願中）
+        await getAndSetPokemonThunk()(vi.fn(), vi.fn(), vi.fn())
+
+        expect(axios.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/1', {timeout: 2000})
+    })
+
+    test('getAndSetPokemonThunkを呼んだ時、payloadの値が正しい値になっていること', async () => {
+        vi.mocked(axios.get).mockResolvedValue({
+            data: {
+                name: 'hoge1',
+                sprites: {
+                    other: {
+                        showdown: {
+                            front_default: 'hoge2'
+                        }
+                    }
+                }
+            }
+        })
+
+        const result = await getAndSetPokemonThunk()(vi.fn(), vi.fn(), vi.fn())
+
+        expect(result.payload).toEqual({
+            "imageUrl": "hoge2",
+            "name": "hoge1",
+            "status": "fulfilled",
+        })
+    })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
+import { InlineConfig } from 'vitest'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
+const testConfig: InlineConfig = {
+  environment: 'jsdom',
+  setupFiles: ['test-setup.js'],
+  globals: true,
+  include: ['./src/**/*.test.{tsx,ts}'],
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  test: testConfig,
+  build: {
+    outDir: "../backend/src/main/resources/static/",
+  },
 })
